decf38c715824459b9e1f17eec5b7bfb2db36ccc
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-03-27 01:31:09 -0300

    exercicios feitos

---
+++ modulo-d/ex001.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60 ) {
+    console.log(`Você ultrapassou a velocidade permitida. Você foi multado!`)
+}
+console.log(`Você está dirigindo abaixo do limite de velocidade, continue assim.`)
