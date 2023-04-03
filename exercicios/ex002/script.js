b7b45173cd7c3f8b5d4c10a3e38a3a50b35df149
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-02 23:52:47 -0300

    imagens agora funcionando

--- exercicios/ex002/script.js
+++ exercicios/ex002/script.js
@@ -15,31 +15,31 @@ function verificar() {
             gênero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //criança
-                img.setAttribute('src', 'crianca.png')
-            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/crianca.png')
+            } else if (idade < 25) {
                 //jovem
-                img.setAttribute('src', 'jovem-masc.png')
-            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/jovem-masc.png')
+            } else if (idade < 60) {
                 //adulto
-                img.setAttribute('src', 'adulto_masc.png')
+                img.setAttribute('src', 'imagens/adulto_masc.png')
             } else {
                 //idoso
-                img.setAttribute('src', 'idoso_masc.png')
+                img.setAttribute('src', 'imagens/idoso_masc.png')
             }
         } else if (sex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 //criança
-                img.setAttribute('src', 'crianca_fem.png')
-            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/crianca_fem.png')
+            } else if (idade < 25) {
                 //jovem
-                img.setAttribute('src', 'adolescente_fem.png')
-            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/adolescente_fem.png')
+            } else if (idade < 60) {
                 //adulto
-                img.setAttribute('src', 'adulta_fem.png')
+                img.setAttribute('src', 'imagens/adulta_fem.png')
             } else {
                 //idoso
-                img.setAttribute('src', 'idosa_fem.png')
+                img.setAttribute('src', 'imagens/idosa_fem.png')
             }
         }
         res.innerHTML = `${gênero} com ${idade} anos`
