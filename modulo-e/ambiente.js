dac39f79515d2307829649f028a5981881d9f132
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-03 15:36:05 -0300

    modulo-e feito

---
+++ modulo-e/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Contando ${c}...`)
+    c++ //c = c + 1
+} while (c <= 20)
+
+/*var c = 1
+while (c <= 20) {
+    console.log(`Contando ${c}...`)
+    c++ //c = c + 1
+}
+*/
+
+/*
+console.log('Contando 1')
+console.log('Contando 2')
+console.log('Contando 3')
+console.log('Contando 4')
+console.log('Contando 5')
+console.log('Contando 6')
+*/
---
+++ modulo-e/script.js
@@ -0,0 +1,5 @@
+console.log('Iniciando')
+for (var c = 1;c <= 10;c++){
+    console.log(`Contando  esses pequenos ${c}...`)
+}
+console.log('Finalizando!')
