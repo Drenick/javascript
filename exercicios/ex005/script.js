0dd545f545b8f250f507936850f8f55261cb1720
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-07 02:13:27 -0300

    att

--- exercicios/ex005/script.js
+++ exercicios/ex005/script.js
@@ -4,17 +4,32 @@ function adicionar(n) {
     var res = document.getElementById('res')
     if (val.value.length == 0) {
         window.alert('Digite algum valor abaixo')
-    } else  {
+    } else {
         n = Number(val.value)
         var num = [val]
     }
-    for(var x = n;x <=100;x += val) {
+    for (var c = n;c <=100;c += val) {
         var item = document.createElement('option')
+        i = Number(item.value)
         item.text = `Valor ${n} adicionado`
-        item.value = `val${x}`
+        item.value = `val${c}`
         tab.appendChild(item)
+    } if (val == i) {
+        window.alert('O numero ja foi colocado')
     }
+   
     if (val.value.length > 100) {
         window.alert('O valor ja foi escrito ou é invalido')
     }
+    
+
 }
+function finalizar() {
+    var fin = document.getElementById('fin')
+    fin.innerHTML = `Ao todo, temos ${num} números cadastrados`
+    
+}    res.innerHTML = `Ao todo, temos ${n} números cadastrados`
+console.log(`O valor do array é ${n}`)
+    console.log(`O vetor tem ${n.length} elemensot/posições`)
+    console.log(`O primeiro valor do array é ${n[0]}`)
+    console.log(`Ordem crescente ${n.sort()}`)
