0591c64dd6aaea44f65298f0fd816f5dc24c856c
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-17 23:11:29 -0300

    circulo

---
+++ testes/função/ex005/circulo.js
@@ -0,0 +1,13 @@
+var r = 4
+function Area(r) {
+    var Area = 3.14 * (r*r)
+    return Area
+}
+console.log(`Área do circulo ${Area(r)}`)
+
+function Perimetro(r) {
+    var r = 2
+    var Perimetro = 2* 3.14*r
+    return Perimetro
+}
+console.log(`Circunferência/Perímetro ${Perimetro(r)}`)
