00317f091beaff40cb23d99e246c459fc8f673fb
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-20 01:08:16 -0300

    alterações do formato do exercício

--- testes/exercicios/ex001/script.js
+++ testes/exercicios/ex001/script.js
@@ -3,6 +3,7 @@ function verificar() {
     var passageiro = document.getElementById('pass')
     var assentos = document.getElementById('asse')
     var onibus = document.getElementById('onib')
+    var contagem = 0
     var res = document.getElementById('res')
     if (passageiro.value.length == 0 || onibus.value.length == 0) {
         res.innerHTML = 'Preencha os campos acima'
@@ -13,15 +14,13 @@ function verificar() {
         var onib = Number(onibus.value)
         if (pass <= 0) {
             res.innerHTML = 'O ônibus não irá sair sem nenhum passageiro.'
-        } else if (pass > asse) {
-            for (var cp = pass; cp <= asse; cp++)
-            var fespera = pass - (asse*onib)
-            res.innerHTML = `A contagem dos passageiros irá começar ${cp}, os ${fespera} terão que esperar pelos próximos ônibus.`
-        } else {
-            for (var cp = 1; cp <= asse; cp++)
-            var osobrou = (asse*onib) - pass
-            res.innerHTML = `A contagem dos passageiros irá começar ${cp}, sobrou ${osobrou} assentos disponíveis dentro do ônibus.`
         }
-        
+        if (asse > pass) {
+            var osobrou = (asse*onib) - pass
+            res.innerHTML = `O total de passageiros embarcados são ${pass}, sobrou ${osobrou} assentos disponíveis dentro do ônibus.`          
+        } else {
+            var fespera = pass - (asse*onib)
+            res.innerHTML = `O total de passageiros embarcados são ${pass}, porém os ônibus suportam apenas ${asse} assentos, os outros ${fespera} tripulantes terão que esperar pelos próximos ônibus.`
+            }
     }
 }
---
+++ testes/for.js
@@ -0,0 +1,5 @@
+var contagem = 0
+
+for (var x = contagem; x <= 10; x++) {
+    console.log(`Contando ${x}`)
+}
