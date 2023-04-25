224ea1c7f5a4bb17c7353d8c4b9751d60b720c89
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-25 00:48:23 -0300

    ex carteira

---
+++ testes/exercicios/condição/carteira.js
@@ -0,0 +1,8 @@
+var idade = 20;
+var ncnh = false;
+var cnh = true;
+if (idade >= 18 && cnh) {
+    console.log('Voce está apto para dirigir.')
+} else {
+    console.log('Voce não está apto para dirigir.')
+}
