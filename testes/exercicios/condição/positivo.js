87614acf768fc82f6695fd40c67f6caa260a3ecb
Nícolas Reis <112213252+nicolasreis1@users.noreply.github.com>
2023-04-24 23:49:09 -0300

    ex voto e positivo

---
+++ testes/exercicios/condição/positivo.js
@@ -0,0 +1,8 @@
+var n = 10
+if (n > 0) {
+    console.log(`O resultado é positivo`)
+} else if (n == 0) {
+    console.log(`O resultado é zero`)
+} else {
+    console.log(`O resultado é negativo`)
+}
